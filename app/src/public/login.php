206fd275673576b671d6b66209fcd46e9e6d5d35
kandalog <asuasu.tee@gmail.com>
2024-12-17 14:35:38 +0900

    rename

--- app/src/public/login.php
+++ app/src/public/login.php
@@ -1,9 +1,33 @@
 <?php
 session_start();
-$err = $_SESSION;
+require_once '../classes/UserLogic.php';
 
-$_SESSION = [];
-session_destroy();
+$err = [];
+
+if (!$email = filter_input(INPUT_POST, 'email')) {
+  $err['email'] = 'ユーザー名を記入してください';
+}
+
+if (! $password = filter_input(INPUT_POST, 'password')) {
+  $err['password'] = 'パスワードを入力してください';
+}
+
+if (count($err) > 0) {
+  // エラーがあった場合は戻す
+  $_SESSION = $err;
+  header('Location: login_form.php');
+  return;
+}
+// ログイン成功時の処理
+$result = UserLogic::login($email, $password);
+
+// ログイン失敗時の処理
+if (!$result) {
+  header('Location: login.php');
+  return;
+}
+echo 'ログイン成功です';
+
 ?>
 
 <!DOCTYPE html>
@@ -16,30 +40,13 @@ session_destroy();
 </head>
 
 <body>
-  <h2>ログインフォーム</h2>
-  <?php if (isset($err['msg'])) :  ?>
-    <p><?php echo $err['msg']; ?></p>
+  <?php if (count($err) > 0) : ?>
+    <?php foreach ($err as $e) : ?>
+      <p><?php echo $e ?></p>
+    <?php endforeach ?>
   <?php endif ?>
-  <form action="top.php" method="POST">
-    <p>
-      <label for="email">メールアドレス：</label>
-      <input type="text" name="email">
-      <?php if (isset($err['email'])) :  ?>
-    <p><?php echo $err['email']; ?></p>
-  <?php endif ?>
-  </p>
-  <p>
-    <label for="password">パスワード：</label>
-    <input type="password" name="password">
-    <?php if (isset($err['password'])) :  ?>
-  <p><?php echo $err['password']; ?></p>
-<?php endif; ?>
-</p>
-<p>
-  <input type="submit" value="ログイン">
-</p>
-  </form>
-  <a href="signup_form.php">新規登録はこちら</a>
+  <p>ユーザ登録が完了しました</p>
+  <a href="./signup_form.php">戻る</a>
 </body>
 
 </html>
